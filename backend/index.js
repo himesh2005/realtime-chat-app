64a6323d40ad69fe05cb3a476bdf9af783944e77
abhi <himeshchakrapani6@gmail.com>
2026-01-15 01:36:34 +0530

    Day -6 : Add Mongo DB auth with JWT and protected route

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,32 @@
+require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
-const { v4: uuidv4 } = require("uuid")
-
+const mongoose = require("mongoose");
 const app = express()
+const auth = require("./middleware/auth.js");
+
+
 app.use(cors())
 app.use(express.json())
 
-// test route
 app.get("/", (req, res) => {
   res.send("Backend is running")
 })
 
-// login route
-app.post("/login", (req, res) => {
-  const { email } = req.body
-  if (!email) {
-    return res.status(400).json({ error: "Email required" })
-  }
+app.get("/api/me", auth, (req, res) => {
+  res.json({ message: "You are authorized", userId: req.userId });
+});
 
-  const userId = uuidv4()
-  res.json({ userId, email })
-})
+const authRoutes = require("./routes/auth");
+
+app.use("/api/auth", authRoutes);
 
 const PORT = 5000
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+  const header = req.headers.authorization;
+  if (!header) return res.status(401).json({ message: "No token" });
+
+  const token = header.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Invalid token format" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Token invalid" });
+  }
+}
+
+module.exports = auth;
